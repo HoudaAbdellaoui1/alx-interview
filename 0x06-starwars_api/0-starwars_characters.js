b7ec5398a988eda87e52e913cb066f93a8f7dd12
root <root@PETEALENS001>
2024-11-17 21:15:51 +0100

    start with the right import

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,49 @@
 #!/usr/bin/node
-const axios = require('axios');
+const fetch = require('node-fetch');
 
-if (process.argv.length < 3) {
-  console.error("Usage: ./script.js <Movie ID>");
-  process.exit(1);
+async function getMovieCharacters(movieId) {
+    // Base URL for the Star Wars API
+    const baseUrl = 'https://swapi-api.alx-tools.com/api';
+    
+    try {
+        // Get the movie data
+        const movieResponse = await fetch(`${baseUrl}/films/${movieId}`);
+        
+        if (!movieResponse.ok) {
+            throw new Error(`Error: Could not fetch movie with ID ${movieId}`);
+        }
+        
+        const movieData = await movieResponse.json();
+        const characterUrls = movieData.characters;
+        
+        // Fetch and print each character
+        for (const charUrl of characterUrls) {
+            const charResponse = await fetch(charUrl);
+            
+            if (charResponse.ok) {
+                const character = await charResponse.json();
+                console.log(character.name);
+            }
+        }
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
 }
 
-const movieId = process.argv[2];
-const baseUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-// Fetch the movie data
-axios.get(baseUrl)
-  .then(response => {
-    const movieData = response.data;
-
-    if (!movieData.title) {
-      console.error("Movie not found.");
-      process.exit(1);
-    }
-
-    console.log(`Characters in '${movieData.title}':`);
-
-    // Fetch and print each character's name
-    const characterPromises = movieData.characters.map(url => axios.get(url));
-    return Promise.all(characterPromises);
-  })
-  .then(characterResponses => {
-    characterResponses.forEach(res => {
-      console.log(res.data.name);
-    });
-  })
-  .catch(error => {
-    console.error(`Error: ${error.message}`);
+// Check if movie ID was provided
+if (process.argv.length !== 3) {
+    console.log('Usage: ./script.js <movie_id>');
     process.exit(1);
-  });
+}
+
+// Get and validate movie ID
+const movieId = parseInt(process.argv[2]);
+
+if (isNaN(movieId)) {
+    console.log('Error: Movie ID must be a number');
+    process.exit(1);
+}
+
+// Run the main function
+getMovieCharacters(movieId);
