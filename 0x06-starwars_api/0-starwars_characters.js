eb7a21d2d06003403508672e4f69e8affa26d56b
root <root@PETEALENS001>
2024-11-17 21:18:47 +0100

    start with the right import

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,34 +1,39 @@
 #!/usr/bin/node
-const fetch = require('node-fetch');
+const request = require('request');
 
-async function getMovieCharacters(movieId) {
-    // Base URL for the Star Wars API
+function getMovieCharacters(movieId) {
     const baseUrl = 'https://swapi-api.alx-tools.com/api';
     
-    try {
-        // Get the movie data
-        const movieResponse = await fetch(`${baseUrl}/films/${movieId}`);
-        
-        if (!movieResponse.ok) {
-            throw new Error(`Error: Could not fetch movie with ID ${movieId}`);
+    // Get the movie data
+    request(`${baseUrl}/films/${movieId}`, function (error, response, body) {
+        if (error || response.statusCode !== 200) {
+            console.error(`Error: Could not fetch movie with ID ${movieId}`);
+            process.exit(1);
         }
-        
-        const movieData = await movieResponse.json();
+
+        const movieData = JSON.parse(body);
         const characterUrls = movieData.characters;
         
-        // Fetch and print each character
-        for (const charUrl of characterUrls) {
-            const charResponse = await fetch(charUrl);
-            
-            if (charResponse.ok) {
-                const character = await charResponse.json();
-                console.log(character.name);
-            }
-        }
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-    }
+        // Counter to keep track of completed requests
+        let completed = 0;
+        
+        // Process each character URL in sequence to maintain order
+        characterUrls.forEach((charUrl, index) => {
+            request(charUrl, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    const character = JSON.parse(body);
+                    // Store character with original index to maintain order
+                    characters[index] = character.name;
+                }
+                completed++;
+                
+                // Print all characters in order when all requests are done
+                if (completed === characterUrls.length) {
+                    characters.forEach(name => console.log(name));
+                }
+            });
+        });
+    });
 }
 
 // Check if movie ID was provided
@@ -44,6 +49,9 @@ if (isNaN(movieId)) {
     console.log('Error: Movie ID must be a number');
     process.exit(1);
 }
+
+// Array to store characters in correct order
+const characters = [];
 
 // Run the main function
 getMovieCharacters(movieId);
